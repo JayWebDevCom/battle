ba40280746319ee38912adde775eb4febbbfc386
JayWebDevCom <jaiye.s@gmail.com>
2017-04-25 20:37:02 +0100

    add second working test

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,6 +1,17 @@
 feature 'Testing infrastructure' do
-  scenario 'Can run app and check page content' do
+
+  scenario 'player name checking' do
     visit('/')
-    expect(page).to have_content 'Hello Battle!'
+    expect(page).to have_content 'Enter Player Names'
   end
+
+  scenario 'players enter their names' do
+    visit('/')
+    fill_in 'player_1', with: 'Joffrey'
+    fill_in 'player_2', with: 'Stannis'
+    click_on('To Battle!')
+    expect(page).to have_content 'Stannis'
+    expect(page).to have_content 'Joffrey'
+  end
+
 end
