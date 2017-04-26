cc20611235d4acbad18d26f2686c24c91acef518
JayWebDevCom <jaiye.s@gmail.com>
2017-04-26 13:57:44 +0100

    add hit points and confirmation

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,10 +1,19 @@
 feature 'Playability' do
+
   before do
     sign_in_and_play
   end
-scenario 'View hit points and names' do
+
+  it 'View hit points and names' do
   expect(page).to have_content 'Stannis : 100HP'
   expect(page).to have_content 'Joffrey : 100HP'
+  end
+
+  it 'Hit points and confirmation' do
+    click_link('Attack-Player!')
+    expect(page).to have_content 'Joffrey : 100HP'
+    expect(page).to have_content 'Stannis : 75HP'
+    expect(page).to have_content 'Joffrey attacked Stannis'
   end
 
 end
