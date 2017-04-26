35d32f32297f87ac44373938abdb5a5124fb7b6d
JayWebDevCom <jaiye.s@gmail.com>
2017-04-26 12:21:24 +0100

    add player hitpoints

--- Gemfile
+++ Gemfile
@@ -2,5 +2,6 @@
 source 'https://rubygems.org'
 
 gem 'capybara'
+gem 'rack'
 gem 'rspec'
 gem 'sinatra'
--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,4 +1,4 @@
-feature 'Testing infrastructure' do
+feature 'Playability' do
 
   scenario 'players enter their names' do
     visit('/')
@@ -7,6 +7,15 @@ feature 'Testing infrastructure' do
     click_on('To Battle!')
     expect(page).to have_content 'Stannis'
     expect(page).to have_content 'Joffrey'
+  end
+
+  scenario 'View hit points' do
+    visit('/')
+    fill_in 'player_1', with: 'Joffrey'
+    fill_in 'player_2', with: 'Stannis'
+    click_on('To Battle!')
+    expect(page).to have_content 'Stannis : 100HP'
+    expect(page).to have_content 'Joffrey : 100HP'
   end
 
 end
