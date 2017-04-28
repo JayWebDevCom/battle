4cf39ea2eddec5517fd7a59ce0d9353d075994f8
JayWebDevCom <jaiye.s@gmail.com>
2017-04-28 09:21:01 +0100

    add controller refactor

--- app.rb
+++ app.rb
@@ -4,6 +4,10 @@ require './lib/game'
 
 class Battle < Sinatra::Base
 
+  before do
+    @game = Game.the_game_instance
+  end
+
   get '/' do
     erb(:index)
   end
@@ -12,26 +16,23 @@ class Battle < Sinatra::Base
     player_1 = Player.new(params[:player_1])
     player_2 = Player.new(params[:player_2])
     Game.playable_game(player_1, player_2)
-    @game = Game.the_game_instance
     redirect '/play'
   end
 
   get '/play' do
-    @game = Game.the_game_instance
     erb(:play)
   end
 
   get '/attack' do
-    @game = Game.the_game_instance
     @game.who_is_up
-    defend = @game.attacking_order[1]
-    @game.get_attacked(defend)
-    redirect '/winner' if @game.player_1.hp == 0 || @game.player_2.hp == 0
+    @defender = @game.attacking_order[1]
+    @attacker = @game.attacking_order[0]
+    @game.get_attacked(@defender)
+    redirect '/winner' if @attacker.hp == 0 || @defender.hp == 0
     erb(:attack)
   end
 
   get '/winner' do
-    @game = Game.the_game_instance
     erb(:winner)
   end
 
